dce2982be05d70b4c72a2bcd88df14152008c649
Luiz Soares <dev.soares@outlook.com>
2020-04-14 20:04:29 -0300

    Added LoginController

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Teste02.Models;
+using Teste02.Services;
 
 namespace Teste02.Controllers
 {
@@ -11,5 +11,44 @@ namespace Teste02.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly UserManagementService _userManagementService;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginController(UserManagementService userManagementService,
+                                SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManagementService = userManagementService;
+        }
+
+        [HttpPost("Registrar")]
+        public async Task<ActionResult> Registrar(UserRegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+
+            var result = await _userManagementService.Create(viewModel);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            viewModel.Senha = string.Empty;
+            viewModel.ConfirmaSenha = string.Empty;
+
+            return Ok(viewModel);
+        }
+
+        [HttpPost("Entrar")]
+        public async Task<ActionResult> Login(UserLoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Senha, false, true);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Usuário ou senha inválido.");
+            }
+
+            return Ok(await _userManagementService.GerarJWT(viewModel.Email));
+        }
     }
 }
