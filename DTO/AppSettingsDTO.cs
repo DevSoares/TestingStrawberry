70b776502d3488db81b607c220683073a664c5f7
Luiz Soares <dev.soares@outlook.com>
2020-04-14 16:45:04 -0300

    Added Initial project

---
+++ DTO/AppSettingsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teste02.DTO
+{
+    public class AppSettingsDTO
+    {
+        public string Secret { get; set; }
+        public int ExpirationTime { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
