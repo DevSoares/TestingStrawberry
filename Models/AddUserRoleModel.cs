48f5a55ac2dda650389434924177aca2e99924ef
Luiz Soares <dev.soares@outlook.com>
2020-04-16 20:22:29 -0300

    Added two models that helps AuthorizationController

---
+++ Models/AddUserRoleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Teste02.Models
+{
+    public class AddUserRoleModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ Models/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Teste02.Models
+{
+    public class RoleViewModel
+    {
+        [Required]
+        public string Nome { get; set; }
+    }
+}
